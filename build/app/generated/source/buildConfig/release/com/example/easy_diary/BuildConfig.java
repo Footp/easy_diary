d2fedf3f0528d65cea12d75ec27af3729a807916
ihwani <117959071+Footp@users.noreply.github.com>
2022-12-05 16:18:22 +0900

    내부테스트용 앱번들

---
+++ build/app/generated/source/buildConfig/release/com/example/easy_diary/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.easy_diary;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.darn_think.easy_diary";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
